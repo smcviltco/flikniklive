525be5a9b9807e12b76b18b95818ccdec9b8145b
LAHORETS <58694110+Viltco@users.noreply.github.com>
2023-01-03 13:37:32 +0500

    wa

--- create_invoice_replica/models/__init__.py
+++
@@ -1,7 +0,0 @@
-# -*- coding: utf-8 -*-
-
-from . import models
-from . import invoice_wizard
-from . import sale_wizard
-from . import purchase_wizard
-from . import payment_wizard
--- partner_ledger_multicurrency/wizard/__init__.py
+++
@@ -1,1 +0,0 @@
-from . import wizard_partner_ledger
